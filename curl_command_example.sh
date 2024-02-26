943fdf82fc36c0e0b98e493ff2d310df31a732ea
Emiliano G Solazzi Griminger <133587754+Enertopia@users.noreply.github.com>
2024-02-26 15:17:19 -0500

    Update curl_command_example.sh

--- curl_command_example.sh
+++ curl_command_example.sh
@@ -1,1 +1,1 @@
-curl -X POST -H "Content-Type: application/json" -d '{"amount_fiat": 100, "conversion_percentage": 0.05}' http://localhost:3000/convert
+curl -X POST -H "Content-Type: application/json" -d '{"amountfiat": 100, "conversionpercentage": 0.05}' http://localhost:3000/convert
